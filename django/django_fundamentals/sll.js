5af4cb049dab16f339d63461f815c37857210472
Maxine Causevic <maxine@fitmaxpersonaltraining.com>
2021-03-19 11:20:08 -0500

    commit

--- django/django_fundamentals/sll.js
+++ django/django_fundamentals/sll.js
@@ -110,3 +110,119 @@ class SinglyLinkedList {
 
 // SLList: Move Max to Back
 // Create a function that moves the node with the maximum value to the tail of a singly linked list.
+
+// SLList: Prepend Value
+// Given a value and a target value, create a node containing value and insert it before the node containing the target value in a singly linked list.
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+    }
+    prependValue(val){
+        let runner = this.head;
+        let val = this.head;
+        let target = this.tail;
+
+        while(runner){
+            if(runner.val = runner.next)
+                runner = this.tail;
+                runner = target;
+
+
+        }
+    }
+
+
+// SLList: Append Value
+// Given a value and a target value, create a node containing value and insert it after the node containing the target value in a singly linked list.
+
+// SLList: Remove Negatives
+// Create a function that removes all negative values from a list, then return the modified list.
+
+// Second to Last Value
+// Create a function that returns the value in the second-to-last node.
+
+// Partition
+// Create a function that, given a value, locates the first node with that value, moves all nodes with values less than that value to be earlier, and moves all nodes with values greater than that value to be later. Otherwise, original order need not be perfectly preserved. Return the list when complete.
+
+// lowerList, higherList
+// Traverse the list and 
+
+// 2 > 8 > 1 > 4 > 10 > 9 > 5
+// 2 > 1 > 4 > 8 > 10 > 9 > 5
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    populateRandom(max, length) {
+        for (let i = 1; i <= length; i++) {
+            let temp = Math.floor(Math.random() * max) + 1;
+            this.addFront(temp);
+        }
+        return this;
+    }
+
+    addFront(val) {
+        const newHead = new Node(val);
+        newHead.next = this.head;
+        this.head = newHead;
+        return this;
+    }
+
+    contains(val) {
+        if (this.isEmpty()) {
+            console.log("This SLL is empty.")
+            return this;
+        } else {
+            let runner = this.head;
+            while (runner) {
+                if (runner.val === val) {
+                    console.log(`Value (${val}) found.`)
+                    return true;
+                }
+                runner = runner.next;
+            }
+            console.log(`Value (${val}) not found.`)
+            return false;
+        }
+    }
+
+    printVals() {
+        if (this.isEmpty()) {
+            console.log("This SLL is empty.")
+            return this;
+        } else {
+            let runner = this.head;
+            let output = `**********\n`
+            while (runner) {
+                output += `${runner.val.toString()} > `;
+                runner = runner.next;
+            }
+            console.log(output);
+        }
+        return this;
+    }
+}
+//Second to Last Value
+// Create a function that returns the value in the second-to-last node.
+    secondTolast(val){
+        let runner = this.head;
+        let secondtolast = runner.next.next-runner.next;
+        if (runner.val === secondtolast.val){
+            console.log(`second to last value (${val}) found.`);
+            return true;
+        }
+    }
